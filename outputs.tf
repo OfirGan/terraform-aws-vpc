a3306c9b7341f8c892c6516e1307dd6b995610cc
Ofir Gan <offirgan@gmail.com>
2021-12-26 22:00:23 +0200

    remove unneeded output

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "available_zone_names" {
-  value = data.aws_availability_zones.available.names
-}
-
 output "public_subnets_ids" {
   value = aws_subnet.public_subnets.*.id
 }
